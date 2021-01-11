75dd6a5d05c9d53cd551cfecd7834a5b4db13bc4
Animesh Bhakat <bhakat.animesh1024@gmail.com>
2021-01-11 20:45:54 +0530

    Created hello world in C-sharp

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Local Hack Day Geeks! ");
+			Console.WriteLine("This Is Animesh from India. This is my first participation in any MLH programme and I am loving this!!!");
+        }
+    }
+}
